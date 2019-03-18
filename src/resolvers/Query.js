1203114187e40439e75e964905742e677cdcadc1
dpyzo0o <yaxu@student.ethz.ch>
2019-03-18 15:14:06 +0100

    authentication

---
+++ src/resolvers/Query.js
@@ -0,0 +1,12 @@
+function info() {
+  return 'This is the API of a Hackernews Clone'
+}
+
+function feed(parent, args, context, info) {
+  return context.prisma.links()
+}
+
+module.exports = {
+  info,
+  feed
+}
